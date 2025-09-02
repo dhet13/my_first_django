c8c46883a31ee24f0febcdb6bb73815b9d1c8c1a
dhet <reussiter13@gmail.com>
2025-09-02 13:25:48 +0900

    merge 내용 삭제

--- polls/admin.py
+++ polls/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from .models import Article, Memo
 
-<<<<<<< HEAD
 # Register your models here.
-# @admin.register(Article)
+@admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content_short','created_at')
+    list_display = ('title', 'content_short', 'created_at', 'updated_at')
+    search_fields = ['title', 'content']
+    list_filter = ['created_at']
 
     def content_short(self, article):
         return article.content[:10]
     content_short.short_description = '간략한 내용'
-
-admin.site.register(Article, ArticleAdmin)
 
 # admin.py에서 우리가 만든 모델을 등록하는 것을 해보겠습니다!
 # 샘플도 확인하시고 다른 툴(제미나이, gpt)을 이용해서 구조를 만들어서 적용도 해보겠습니다
@@ -23,21 +22,10 @@ admin.site.register(Article, ArticleAdmin)
 
 # DB초기화 
 # sqlite파일 삭제, migrations 내부 파일 삭제(__init__.py는 있어야합니다!!)
-=======
-@admin.register(Article)
-class AritcleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_at', 'updated_at']
-    search_fields = ['title', 'content']
-    list_filter = ['created_at']
->>>>>>> d08acb3d802c4d43e517fb5a0c8d9db6c115538e
 
 @admin.register(Memo)
 class MemoAdmin(admin.ModelAdmin):
     list_display = ['title', 'is_important', 'created_at']
     list_filter = ['is_important', 'created_at']
     search_fields = ['title', 'content']
-<<<<<<< HEAD
     list_editable = ['is_important']  # 목록에서 바로 수정 가능!
-=======
-    list_editable = ['is_important']
->>>>>>> d08acb3d802c4d43e517fb5a0c8d9db6c115538e
